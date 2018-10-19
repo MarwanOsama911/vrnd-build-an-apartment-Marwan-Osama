205082dd4aabd9fc87eede0bda97513ce38e7f0c
Marwan Osama <MarwanOsama1997@gmail.com>
2018-10-19 05:42:16 +0200

    VR

---
+++ Assets/Scripts/HeadRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Input.gyro.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion at = Input.gyro.attitude;
+		at = Quaternion.Euler (90f, 0f, 0f) * new Quaternion (at.x, at.y, -at.z, -at.w);
+		transform.rotation = at;
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vrnd-build-an-apartment-Marwan-Osama
+*Unity version : Unity LTS Release 2017.4.4
+*GVR SDK for Unity: GVR v1.100.1
